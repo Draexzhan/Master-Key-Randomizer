f0aa280a753acb01385b46c21a0bb4e47f227344
Draexzhan <nathan.gaponoff@gmail.com>
2024-08-21 21:18:59 -0400

    reverted saves to no longer using the % symbol

--- MasterKeyRandoMenu.cs
+++ MasterKeyRandoMenu.cs
@@ -19,7 +19,7 @@ public class RandomizerEditor : MonoBehaviour
     public static int KeysanityLogic = PlayerPrefs.GetInt("KeysanityLogic", 0);
     public static int DarkOreHuntLogic = PlayerPrefs.GetInt("DarkOreHuntLogic", 0);
     public static int BossKillLogic = PlayerPrefs.GetInt("BossKillLogic", 0);
-    public static int DoSeedPreset = PlayerPrefs.GetInt("DoSeedPreset", 0);
+    public static int DoSeedPreset = PlayerPrefs.GetInt("DoSeedPreset", 1);
     public static int WeaponLogic = PlayerPrefs.GetInt("WeaponLogic", 0);
     public static int VanillaBossKeys = PlayerPrefs.GetInt("VanillaBossKeys", 0);
     public static int WarpShuffle = PlayerPrefs.GetInt("WarpShuffle", 1);
@@ -36,7 +36,7 @@ public class RandomizerEditor : MonoBehaviour
             Cursor.visible = true;
             if (expanded)
             {
-                if (PlayerPrefs.GetInt("randomizerSlot" + saveNumber + "%maxArgent") == 0)
+                if (PlayerPrefs.GetInt("randomizerSlot" + saveNumber + "maxArgent") == 0)
                     GUI.Window(101, new Rect(10f, 10f, 500f, 600f), new GUI.WindowFunction(RandomizerSettingsWindow), "Randomizer Settings");
 				else
 					GUI.Window(101, new Rect(10f, 10f, 500f, 600f), new GUI.WindowFunction(RandomizerStatsWindow), "Randomizer Settings");
@@ -159,8 +159,8 @@ public class RandomizerEditor : MonoBehaviour
 		{
 			//EraseConfirm = GUI.Button(new Rect(130f, 400f, 150f, 30f), "Are you sure?");
 			LoadSceneOnClick SaveLoader = FindObjectOfType<LoadSceneOnClick>();
-            SaveLoader.saveslot = "randomizerSlot" + saveNumber + "%";
-            PlayerPrefs.SetInt("randomizerSlot" + saveNumber + "%randoSeed", default);
+            SaveLoader.saveslot = "randomizerSlot" + saveNumber;
+            PlayerPrefs.SetInt("randomizerSlot" + saveNumber + "randoSeed", default);
 			foreach (string key in CheckClass.CheckLookup.Locations.Keys)
 				PlayerPrefs.DeleteKey(SaveLoader.saveslot + key);
             SaveLoader.eraseSave();
@@ -169,13 +169,15 @@ public class RandomizerEditor : MonoBehaviour
         {
             RandoMode();
             LoadSceneOnClick SaveLoader = FindObjectOfType<LoadSceneOnClick>();
-            SaveLoader.saveslot = "randomizerSlot" + saveNumber + "%";
+            SaveLoader.saveslot = "randomizerSlot" + saveNumber;
 			if (int.TryParse(seedPresetString, out var result2))
 			{
 				seedPreset = result2;
 			}
 			PlayerPrefs.SetInt("SeedPreset", seedPreset);
 			PlayerPrefs.SetInt("RandoLastWorld", saveNumber);
+			if (PlayerPrefs.GetInt("StartLogic") == 1)
+				PlayerPrefs.SetString(SaveLoader.saveslot + "respawn", "village");
             SaveLoader.LoadSceneAndPlay("OverWorld");
         }
     }
@@ -189,24 +191,24 @@ public class RandomizerEditor : MonoBehaviour
 			saveNumber = result;
 		}
 		GUI.Label(new Rect(10f, 20f, 160f, 40f), "Summary:");
-		GUI.Label(new Rect(10f, 40f, 160f, 40f), "Money: " + PlayerPrefs.GetInt("randomizerSlot" + saveNumber + "%argent", 0) + " / " + PlayerPrefs.GetInt("randomizerSlot" + saveNumber + "%maxArgent", 0));
-		GUI.Label(new Rect(10f, 60f, 160f, 40f), "Crystals: " + PlayerPrefs.GetInt("randomizerSlot" + saveNumber + "%cristals", 0) + " / " + PlayerPrefs.GetInt("randomizerSlot" + saveNumber + "%cristalsGiven", 0));
-		GUI.Label(new Rect(10f, 80f, 160f, 40f), "Weapon Level: " + PlayerPrefs.GetInt("randomizerSlot" + saveNumber + "%picLVL", 0));
-		GUI.Label(new Rect(10f, 100f, 160f, 40f), "Health: " + PlayerPrefs.GetInt("randomizerSlot" + saveNumber + "%PDVActuels", 0) + " / " + PlayerPrefs.GetInt("randomizerSlot" + saveNumber + "%PDVMax", 0));
-		GUI.Label(new Rect(10f, 120f, 100f, 40f), "Armor Level: " + PlayerPrefs.GetInt("randomizerSlot" + saveNumber + "%armor", 0));
-		GUI.Label(new Rect(10f, 140f, 160f, 40f), "Balloon Level: " + PlayerPrefs.GetInt("randomizerSlot" + saveNumber + "%ballon", 0));
-		GUI.Label(new Rect(10f, 160f, 160f, 40f), "Boomerang Level: " + PlayerPrefs.GetInt("randomizerSlot" + saveNumber + "%boomerang", 0));
-		GUI.Label(new Rect(10f, 180f, 160f, 40f), "Boots Level: " + PlayerPrefs.GetInt("randomizerSlot" + saveNumber + "%bottes", 0));
-		GUI.Label(new Rect(10f, 200f, 160f, 40f), "Clover Level: " + PlayerPrefs.GetInt("randomizerSlot" + saveNumber + "%clover", 0));
-		GUI.Label(new Rect(10f, 220f, 160f, 40f), "Gloves Level: " + PlayerPrefs.GetInt("randomizerSlot" + saveNumber + "%creuse", 0));
-		GUI.Label(new Rect(10f, 240f, 160f, 40f), "Grapple Level: " + PlayerPrefs.GetInt("randomizerSlot" + saveNumber + "%distance", 0));
-		GUI.Label(new Rect(10f, 260f, 160f, 40f), "Lantern Level: " + PlayerPrefs.GetInt("randomizerSlot" + saveNumber + "%lanterne", 0));
-		GUI.Label(new Rect(10f, 280f, 160f, 40f), "Lens Level: " + PlayerPrefs.GetInt("randomizerSlot" + saveNumber + "%vision", 0));
-		GUI.Label(new Rect(10f, 300f, 160f, 40f), "Magnet Level: " + PlayerPrefs.GetInt("randomizerSlot" + saveNumber + "%magnet", 0));
-		GUI.Label(new Rect(10f, 320f, 160f, 40f), "Muscle Level: " + PlayerPrefs.GetInt("randomizerSlot" + saveNumber + "%muscle", 0));
-		GUI.Label(new Rect(10f, 340f, 160f, 40f), "Swim Level: " + PlayerPrefs.GetInt("randomizerSlot" + saveNumber + "%palmes", 0));
-		GUI.Label(new Rect(10f, 360f, 160f, 40f), "Gear Pieces: " + (PlayerPrefs.GetInt("randomizerSlot" + saveNumber + "%Gear1", 0) + PlayerPrefs.GetInt("randomizerSlot" + saveNumber + "%Gear2", 0) + PlayerPrefs.GetInt("randomizerSlot" + saveNumber + "%Gear3", 0)));
-		GUI.Label(new Rect(10f, 380f, 160f, 40f), "Seed: " + PlayerPrefs.GetInt("randomizerSlot" + saveNumber + "%randoSeed", 0));
+		GUI.Label(new Rect(10f, 40f, 160f, 40f), "Money: " + PlayerPrefs.GetInt("randomizerSlot" + saveNumber + "argent", 0) + " / " + PlayerPrefs.GetInt("randomizerSlot" + saveNumber + "maxArgent", 0));
+		GUI.Label(new Rect(10f, 60f, 160f, 40f), "Crystals: " + PlayerPrefs.GetInt("randomizerSlot" + saveNumber + "cristals", 0) + " / " + PlayerPrefs.GetInt("randomizerSlot" + saveNumber + "cristalsGiven", 0));
+		GUI.Label(new Rect(10f, 80f, 160f, 40f), "Weapon Level: " + PlayerPrefs.GetInt("randomizerSlot" + saveNumber + "picLVL", 0));
+		GUI.Label(new Rect(10f, 100f, 160f, 40f), "Health: " + PlayerPrefs.GetInt("randomizerSlot" + saveNumber + "PDVActuels", 0) + " / " + PlayerPrefs.GetInt("randomizerSlot" + saveNumber + "PDVMax", 0));
+		GUI.Label(new Rect(10f, 120f, 100f, 40f), "Armor Level: " + PlayerPrefs.GetInt("randomizerSlot" + saveNumber + "armor", 0));
+		GUI.Label(new Rect(10f, 140f, 160f, 40f), "Balloon Level: " + PlayerPrefs.GetInt("randomizerSlot" + saveNumber + "ballon", 0));
+		GUI.Label(new Rect(10f, 160f, 160f, 40f), "Boomerang Level: " + PlayerPrefs.GetInt("randomizerSlot" + saveNumber + "boomerang", 0));
+		GUI.Label(new Rect(10f, 180f, 160f, 40f), "Boots Level: " + PlayerPrefs.GetInt("randomizerSlot" + saveNumber + "bottes", 0));
+		GUI.Label(new Rect(10f, 200f, 160f, 40f), "Clover Level: " + PlayerPrefs.GetInt("randomizerSlot" + saveNumber + "clover", 0));
+		GUI.Label(new Rect(10f, 220f, 160f, 40f), "Gloves Level: " + PlayerPrefs.GetInt("randomizerSlot" + saveNumber + "creuse", 0));
+		GUI.Label(new Rect(10f, 240f, 160f, 40f), "Grapple Level: " + PlayerPrefs.GetInt("randomizerSlot" + saveNumber + "distance", 0));
+		GUI.Label(new Rect(10f, 260f, 160f, 40f), "Lantern Level: " + PlayerPrefs.GetInt("randomizerSlot" + saveNumber + "lanterne", 0));
+		GUI.Label(new Rect(10f, 280f, 160f, 40f), "Lens Level: " + PlayerPrefs.GetInt("randomizerSlot" + saveNumber + "vision", 0));
+		GUI.Label(new Rect(10f, 300f, 160f, 40f), "Magnet Level: " + PlayerPrefs.GetInt("randomizerSlot" + saveNumber + "magnet", 0));
+		GUI.Label(new Rect(10f, 320f, 160f, 40f), "Muscle Level: " + PlayerPrefs.GetInt("randomizerSlot" + saveNumber + "muscle", 0));
+		GUI.Label(new Rect(10f, 340f, 160f, 40f), "Swim Level: " + PlayerPrefs.GetInt("randomizerSlot" + saveNumber + "palmes", 0));
+		GUI.Label(new Rect(10f, 360f, 160f, 40f), "Gear Pieces: " + (PlayerPrefs.GetInt("randomizerSlot" + saveNumber + "Gear1", 0) + PlayerPrefs.GetInt("randomizerSlot" + saveNumber + "Gear2", 0) + PlayerPrefs.GetInt("randomizerSlot" + saveNumber + "Gear3", 0)));
+		GUI.Label(new Rect(10f, 380f, 160f, 40f), "Seed: " + PlayerPrefs.GetInt("randomizerSlot" + saveNumber + "randoSeed", 0));
 
 		if (GUI.Button(new Rect(10f, 400f, 50f, 30f), "Close"))
 		{
@@ -216,8 +218,8 @@ public class RandomizerEditor : MonoBehaviour
 		{
 			//EraseConfirm = GUI.Button(new Rect(130f, 400f, 150f, 30f), "Are you sure?");
 			LoadSceneOnClick SaveLoader = FindObjectOfType<LoadSceneOnClick>();
-			SaveLoader.saveslot = "randomizerSlot" + saveNumber + "%";
-			PlayerPrefs.SetInt("randomizerSlot" + saveNumber + "%randoSeed", default);
+			SaveLoader.saveslot = "randomizerSlot" + saveNumber;
+			PlayerPrefs.SetInt("randomizerSlot" + saveNumber + "randoSeed", default);
 			foreach (string key in CheckClass.CheckLookup.Locations.Keys)
 				PlayerPrefs.DeleteKey(SaveLoader.saveslot + key);
 			SaveLoader.eraseSave();
@@ -226,7 +228,7 @@ public class RandomizerEditor : MonoBehaviour
 		{
 			RandoMode();
 			LoadSceneOnClick SaveLoader = FindObjectOfType<LoadSceneOnClick>();
-			SaveLoader.saveslot = "randomizerSlot" + saveNumber + "%";
+			SaveLoader.saveslot = "randomizerSlot" + saveNumber;
 			PlayerPrefs.SetInt("RandoLastWorld", saveNumber);
 			SaveLoader.LoadSceneAndPlay("OverWorld");
 		}
@@ -235,7 +237,7 @@ public class RandomizerEditor : MonoBehaviour
 	private static void UpdateLoadedSave()
     {
         LoadSceneOnClick SaveLoader = FindObjectOfType<LoadSceneOnClick>();
-        SaveLoader.saveslot = "randomizerSlot" + saveNumber + "%";
+        SaveLoader.saveslot = "randomizerSlot" + saveNumber;
 		PlayerPrefs.SetInt("SeedPreset", seedPreset);
     }
 }
