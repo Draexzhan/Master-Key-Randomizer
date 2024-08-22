dc090a6d596b82a10e9b35b7bf43e3646b7d1252
Draexzhan <nathan.gaponoff@gmail.com>
2024-08-22 11:31:16 -0400

    Fixed floating rocks in warp zone

--- RockPatcher.cs
+++ RockPatcher.cs
@@ -10,7 +10,7 @@ class RockPatch1
 	[HarmonyPatch(typeof(CassableScript), nameof(CassableScript.Start))]
 	public static void RockPostfix(CassableScript __instance)
 	{
-		if (__instance.transform.parent.name == "bloqueurs" && __instance.tag =="BlockGrappin")
+		if (__instance.transform.parent.name == "bloqueurs" && __instance.tag =="BlockGrappin" && __instance.friable)
 			__instance.transform.position = new Vector3(__instance.origPos.x, 1.2f + __instance.origPos.y, 0f);
 	}
 }
