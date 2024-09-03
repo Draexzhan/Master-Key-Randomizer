f2564bebe15501c08df6ba570bf8f815c184817b
Draexzhan <nathan.gaponoff@gmail.com>
2024-09-02 20:55:26 -0400

    Moved a block in the Factory to prevent softlock

--- BlockPatcher.cs
+++ BlockPatcher.cs
@@ -7,10 +7,14 @@ class BlockPatch1
     [HarmonyPatch(typeof(blocPoussableScript), nameof(blocPoussableScript.Start))]
     public static void BlockPatch(blocPoussableScript __instance)
     {
-        if (__instance.truePos == new UnityEngine.Vector3(-18.5f, 603.5f, -2f) || (__instance.truePos == new UnityEngine.Vector3(-18.5f, 602.5f, -2f)))
+        if (__instance.truePos == new Vector3(-18.5f, 603.5f, -2f) || (__instance.truePos == new Vector3(-18.5f, 602.5f, -2f)))
         {
-            UnityEngine.Object.DestroyImmediate(__instance.GetComponent<GameObject>());
-            __instance.transform.position = __instance.truePos + new UnityEngine.Vector3(-10f, 10f, 0);
+            Object.DestroyImmediate(__instance.GetComponent<GameObject>());
+            __instance.transform.position = __instance.truePos + new Vector3(-10f, 10f, 0);
         }
+        if (__instance.truePos == new Vector3(-33.5f, 603.5f, -2f))
+		{
+			__instance.transform.position = __instance.truePos + new Vector3(-2f, 0f, 0);
+		}
     }
 }
