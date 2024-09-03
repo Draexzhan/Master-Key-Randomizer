d19e7177ecdec241197975a141a7aceea005a5aa
Draexzhan <nathan.gaponoff@gmail.com>
2024-09-03 12:49:41 -0400

    Fixed block ice physics
    Fixed a bug where blocks pushed on ice would stop half a tile from their destination

--- QoLPatcher.cs
+++ QoLPatcher.cs
@@ -1,4 +1,6 @@
 ﻿using HarmonyLib;
+using UnityEngine;
+using static MasterKeyRandomizer.MKLogger;
 
 class QOLPatcher
 {
@@ -13,7 +15,12 @@ class QOLPatcher
 	[HarmonyPatch(typeof(blocPoussableScript), nameof(blocPoussableScript.Start))]
 	public static void FasterBlockPatch(blocPoussableScript __instance)
 	{
+		__instance.transform.position = (Vector3)(Vector2)__instance.transform.position - 2f * Vector3.forward;
+		__instance.truePos = __instance.transform.position;
 		__instance.seuil = 12;
-		__instance.transitionTime = .5f;
+		if (!(__instance.truePos == new Vector3(13.5f, 580.5f, -2f) || __instance.truePos == new Vector3(19.5f, 578.5f, -2f) || __instance.truePos == new Vector3(48.5f, 592.5f, -2f) || __instance.truePos == new Vector3(26.5f, 607.5f, -2f) || __instance.truePos == new Vector3(30.5f, 607.5f, -2f) || __instance.truePos == new Vector3(34.5f, 607.5f, -2f) || __instance.truePos == new Vector3(566.5f, 42.5f, -2)))
+		{
+			__instance.transitionTime = .5f;
+		}
 	}
 }
