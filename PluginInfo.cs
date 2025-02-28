c6a706e6f8b0030e56430da47e1596c7e6436a66
Draexzhan <nathan.gaponoff@gmail.com>
2025-02-28 15:55:40 -0500

    updated version number

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -3,7 +3,7 @@
 	public class MKPluginInfo
 	{
 		public const string NAME = "Master Key Randomizer";
-		public const string VERSION = "0.1.1.3";
+		public const string VERSION = "0.1.1.4";
 		public const string GUID = "com.draexzhan.MasterKeyRandomizer";
 	}
 }
